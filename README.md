a210acd8e4727eb05f5dcc271d42063dbfd9c8d9
Nick <nicholas.chu98@gmail.com>
2021-11-24 08:52:12 -0800

    new files

--- README.md
+++
@@ -1,1 +0,0 @@
-# Web_Practice_Final
---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const PORT = process.env.PORT || 8007;
+const app = express();
+
+// Don't worry about these 4 lines below
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("homepage");
+});
+app.get("/people/:id", (req, res) => {
+  res.render("people");
+});
+
+app.get("/:id/photos", (req, res) => {
+  const id = req.params.id;
+});
+
+app.listen(PORT, () => {
+  console.log(`Server now is running at http://localhost:${PORT} 🚀`);
+});
