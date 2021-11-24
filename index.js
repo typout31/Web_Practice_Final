0d503a7986d7f1be6ff903b5fad039f538e70f3c
Nick <nicholas.chu98@gmail.com>
2021-11-24 09:18:57 -0800

    Added createCard.ejs with some styling

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("homepage");
+  res.render("createCard");
 });
 app.get("/people/:id", (req, res) => {
   res.render("people");
